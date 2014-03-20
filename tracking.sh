048702344d6dd486cca45a859890097a1a700625
Miguel González <migueljgonz@gmail.com>
2014-03-20 23:48:58 +0100

    Temporarily ignore certificate verification issues

--- tracking.sh
+++ tracking.sh
@@ -7,6 +7,6 @@ fi
 
 while read codigo; do
     echo "### Pedido [ $codigo ] ###"
-    curl -s --data "numero=$codigo&accion=LocalizaUno" "https://aplicacionesweb.correos.es/localizadorenvios/track.asp" | tail -n 4 | head -1 | LANG=es_ES.iso88591 sed 's/.*soap:Body.\(.*\)<\/soap:Body.*/\1/g'
+    curl -k -s --data "numero=$codigo&accion=LocalizaUno" "https://aplicacionesweb.correos.es/localizadorenvios/track.asp" | tail -n 4 | head -1 | LANG=es_ES.iso88591 sed 's/.*soap:Body.\(.*\)<\/soap:Body.*/\1/g'
     echo ""
 done < "$1"
