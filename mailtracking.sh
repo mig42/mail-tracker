231222a904b17a527c7b44f934172ae1afde28ca
pando85 <pando85@msn.com>
2014-03-21 02:21:46 +0100

    Cleaning script of rubbish

--- mailtracking.sh
+++ mailtracking.sh
@@ -7,12 +7,11 @@ fi
 
 while read lectura; do codigo=lectura
 done < "$1"
-echo $codigo
+
 
 codigos=$(tr '\n' ' ' < $1)
     echo "### Pedido [ $codigos ] ###"
-    curl -k -s --data "numeros=$codigos&numero=$codigo&ecorreo=$2&accion=LocalizaVarios" "https://aplicacionesweb.correos.es/localizadorenvios/track.asp"
+    curl -k -s --data "numeros=$codigos&numero=$codigo&ecorreo=$2&accion=LocalizaVarios" "https://aplicacionesweb.correos.es/localizadorenvios/track.asp" > /dev/null
 
-
-#  curl -k -s --data "numeros=RF196344966SG RF207323315SG&numero=RF207323315SG&ecorreo=Pando85@msn.com&accion=LocalizaVarios" "https://aplicacionesweb.correos.es/localizadorenvios/track.asp"
+echo "Solicitud enviada"
 
