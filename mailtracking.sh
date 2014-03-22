ff7ec31a06c2a7e468ffef6cbbe63bfd9884fad9
Miguel González <migueljgonz@gmail.com>
2014-03-22 13:24:49 +0100

    Modifying scripts

--- mailtracking.sh
+++ mailtracking.sh
@@ -5,12 +5,11 @@ if [ ! -f "$1" ] || [ ! "$2" ]; then
     exit 1
 fi
 
-while read lectura; do codigo=lectura
-done < "$1"
-
-
-codigos=$(cut -d '#' -d '\n' |tr '\n' ' ' < $1)
-    echo "### Pedido [ $codigos ] ###"
+while read valor; do
+    codigo=`echo $valor | cut -d\# -f1`
+    comentario=`echo $valor | cut -d\# -f2`
+    echo "### Pedido [ $codigo ] - [ $comentario ] ###"
+done < $1
 #    curl -k -s --data "numeros=$codigos&numero=$codigo&ecorreo=$2&accion=LocalizaVarios" "https://aplicacionesweb.correos.es/localizadorenvios/track.asp" > /dev/null
 
 echo "Solicitud enviada"
--- tracking.sh
+++ tracking.sh
@@ -6,11 +6,8 @@ if [ ! -f "$1" ]; then
 fi
 
 while read linea; do
-    read codigo objeto <<<$(IFS="#"; echo $linea)
-    if [ ! "$objeto" ]; then
-        objeto=$codigo
-    fi    
-    echo "### Pedido [ $objeto ] ###"
+    codigo=`echo $linea | cut -d\# -f1`
+    echo "### Pedido [ $codigo ] ###"
     curl -k -s --data "numero=$codigo&accion=LocalizaUno" "https://aplicacionesweb.correos.es/localizadorenvios/track.asp" | tail -n 4 | head -1 | LANG=es_ES.iso88591 sed 's/.*soap:Body.\(.*\)<\/soap:Body.*/\1/g' | python mailTracker.py
     echo ""
 done < "$1"
