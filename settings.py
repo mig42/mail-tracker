eca03e84d438476d8999ea2a729ff03103b4c53a
pando85 <pando85@msn.com>
2014-03-25 19:28:37 +0100

    Added default options in settings.py for variables not listed in settings.cfg

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ class Settings ():
         self._settingParser = ConfigParser.RawConfigParser()
         # TODO: self._settingFilePath = os.path.join('~.mail-tracker', 'settings.cfg')
         self._settingFilePath = os.path.join(os.path.dirname(__file__), 'settings.cfg')
+        self._settingParser = ConfigParser.SafeConfigParser({'username':'mailtrackerpython','password':'oyapulpo','smtp_server':'smtp.gmail.com:587'})
         self._settingParser.read(self._settingFilePath)
         # [mail]
         self.username = self._settingParser.get("mail","username")
