749182a9bc3f9ceb89c55c53ce23fb40a560ba5f
pando85 <pando85@msn.com>
2014-03-25 19:08:35 +0100

    Added TODO in settings.py

--- settings.py
+++ settings.py
@@ -5,13 +5,13 @@ import os
 
 
 
-
 class Settings ():
     def __init__(self):
         self._settingParser = ConfigParser.RawConfigParser()
+        # TODO: self._settingFilePath = os.path.join('~.mail-tracker', 'settings.cfg')
         self._settingFilePath = os.path.join(os.path.dirname(__file__), 'settings.cfg')
         self._settingParser.read(self._settingFilePath)
-        # Email
+        # [mail]
         self.username = self._settingParser.get("mail","username")
         self.password = self._settingParser.get("mail","password")
         self.smtp_server = self._settingParser.get("mail","smtp_server")
