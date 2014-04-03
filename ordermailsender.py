26425ab2508a70408d6320b987f8a90692eabe20
pando85 <pando85@msn.com>
2014-04-03 23:51:35 +0200

    Separate message to send html +  plain_text message

--- ordermailsender.py
+++ ordermailsender.py
@@ -13,7 +13,8 @@ import codecs
 from settings import Settings
 
 
-MESSAGE_FORMAT = u"Subject: Orders info\n\n{0}"
+MESSAGE_FORMAT_1 = u"Subject: Orders info\n\ntext:{0}"
+MESSAGE_FORMAT_2 = u"{0}\n\nhtml:{0}"
 OUTPUT_OK = "Successfully sent email"
 OUTPUT_ERROR = "Error: unable to send email"
 
@@ -30,62 +31,112 @@ class OrderMailSender:
         self._toaddrs = mails
 
     def flush_output(self):
-        with tempfile.NamedTemporaryFile(delete=False) as mail_file:
-            file_name = mail_file.name
-            self.write_mail(mail_file)
+        with tempfile.NamedTemporaryFile(delete=False) as text_mail_file:
+            text_file_name = text_mail_file.name
+            self.text_write_mail(text_mail_file)
 
-        message = self.get_message(file_name)
+        with tempfile.NamedTemporaryFile(delete=False) as html_mail_file:
+            html_file_name = html_mail_file.name
+            self.html_write_mail(html_mail_file)
+
+        message = self.get_message(text_file_name, html_file_name)
 
         if self._verbose:
             print message
 
         self.send_mail(message)
 
-        os.remove(file_name)
+        os.remove(text_file_name)
 
-    def write_mail(self, mail_file):
+    def text_write_mail(self, text_mail_file):
         for order in self._orders:
             if not order.exists():
-                self.print_order_line(
-                    mail_file, "Order '{0}' does not exist.\n", order.get_identifier())
+                self.text_print_order_line(
+                    text_mail_file, "Order '{0}' does not exist.\n", order.get_identifier())
                 continue
 
-            mail_file.write(u"Order '{0}':\n".format(order.get_identifier()).encode("utf-8"))
+            text_mail_file.write(u"Order '{0}':\n".format(order.get_identifier()).encode("utf-8"))
             if len(order.get_events()) == 0:
-                self.print_order_line("  No registered events yet.\n")
+                self.text_print_order_line("  No registered events yet.\n")
                 continue
 
-            self.print_events(mail_file, order.get_events())
+            self.text_print_events(text_mail_file, order.get_events())
 
-    def print_order_line(self, mail_file, text, *args):
+    def html_write_mail(self, html_mail_file):
+        for order in self._orders:
+            if not order.exists():
+                self.html_print_order_line(
+                    html_mail_file, "Order '{0}' does not exist.\n", order.get_identifier())
+                continue
+
+            html_mail_file.write(u"Order '{0}':\n".format(order.get_identifier()).encode("utf-8"))
+            if len(order.get_events()) == 0:
+                self.html_print_order_line("  No registered events yet.\n")
+                continue
+
+            self.html_print_events(html_mail_file, order.get_events())
+
+    def text_print_order_line(self, text_mail_file, text, *args):
         if not self._verbose:
             return
-        mail_file.write(text.format(*args))
+        text_mail_file.write(text.format(*args))
 
-    def print_events(self, mail_file, event_list):
-        self.print_head(mail_file)
+    def html_print_order_line(self, html_mail_file, text, *args):
+        if not self._verbose:
+            return
+        html_mail_file.write(text.format(*args))
+
+    def text_print_events(self, text_mail_file, event_list):
+        self.text_print_head(text_mail_file)
         if self._last_event:
-            self.print_event(mail_file, event_list[-1])
+            self.text_print_event(text_mail_file, event_list[-1])
             return
 
         for event in event_list:
-            self.print_event(mail_file, event)
+            self.text_print_event(text_mail_file, event)
 
-
-    def print_head(self, mail_file):
-        if self._short:
-            mail_file.write(u"  {0: ^20} | {1}\n".format("Date/time", "Status").encode("utf-8"))
+    def html_print_events(self, html_mail_file, event_list):
+        self.html_print_head(html_mail_file)
+        if self._last_event:
+            self.html_print_event(html_mail_file, event_list[-1])
             return
-        mail_file.write( u"  {0: ^20} | {1: ^35} | {2: ^50} | {3}\n".format(
+
+        for event in event_list:
+            self.html_print_event(html_mail_file, event)
+
+    def text_print_head(self, text_mail_file):
+        if self._short:
+            text_mail_file.write(u"  {0: ^20} | {1}\n".format("Date/time", "Status").encode("utf-8"))
+            return
+        text_mail_file.write( u"  {0: ^20} | {1: ^35} | {2: ^50} | {3}\n".format(
             "Date/time", "Status", "Description", "Position").encode("utf-8"))
 
-    def print_event(self, mail_file, event):
+    def html_print_head(self, html_mail_file):
         if self._short:
-            mail_file.write(u"  {0: <20} | {1}\n".format(
+            html_mail_file.write(u"  {0: ^20} | {1}\n".format("Date/time", "Status").encode("utf-8"))
+            return
+        html_mail_file.write( u"  {0: ^20} | {1: ^35} | {2: ^50} | {3}\n".format(
+            "Date/time", "Status", "Description", "Position").encode("utf-8"))
+
+    def text_print_event(self, text_mail_file, event):
+        if self._short:
+            text_mail_file.write(u"  {0: <20} | {1}\n".format(
                 time.strftime(constants.LONG_DATE_FORMAT, event.get_date()),
                 event.get_text()).encode("utf-8"))
             return
-        mail_file.write(u"  {0: <20} | {1: <35} | {2: <50} | {3}\n".format(
+        text_mail_file.write(u"  {0: <20} | {1: <35} | {2: <50} | {3}\n".format(
+            time.strftime(constants.LONG_DATE_FORMAT, event.get_date()),
+            event.get_text(),
+            event.get_description(),
+            event.get_location()).encode("utf-8"))
+
+    def html_print_event(self, html_mail_file, event):
+        if self._short:
+            html_mail_file.write(u"  {0: <20} | {1}\n".format(
+                time.strftime(constants.LONG_DATE_FORMAT, event.get_date()),
+                event.get_text()).encode("utf-8"))
+            return
+        html_mail_file.write(u"  {0: <20} | {1: <35} | {2: <50} | {3}\n".format(
             time.strftime(constants.LONG_DATE_FORMAT, event.get_date()),
             event.get_text(),
             event.get_description(),
@@ -106,6 +157,9 @@ class OrderMailSender:
             if server is not None:
                 server.quit()
 
-    def get_message(self, mail_file_name):
-        with codecs.open(mail_file_name, encoding="utf-8") as mail_file:
-            return MESSAGE_FORMAT.format(mail_file.read())
+    def get_message(self, text_file_name, html_file_name):
+        with codecs.open(text_file_name, encoding="utf-8") as text_mail_file:
+            message = MESSAGE_FORMAT_1.format(text_mail_file.read())
+
+        with codecs.open(html_file_name, encoding="utf-8") as html_mail_file:
+            return MESSAGE_FORMAT_2.format(message, html_mail_file.read())
