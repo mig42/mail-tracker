cec1007f0665322cd151a0183fae39fd0a5a2a7c
Pando85 <pando855@gmail.com>
2015-10-07 19:25:57 +0200

    Updated client parser

--- correosclient.py
+++ correosclient.py
@@ -4,13 +4,13 @@ import sys
 import time
 from urllib import urlencode
 import urllib2
-import xml.etree.ElementTree as ElementTree
+from BeautifulSoup import BeautifulSoup
 
 import order
 import event
 
 
-WEB_SERVICE_ENDPOINT = "https://aplicacionesweb.correos.es/localizadorenvios/track.asp"
+WEB_SERVICE_ENDPOINT = "http://aplicacionesweb.correos.es/localizadorenvios/track.asp"
 
 
 class CorreosClient:
@@ -29,11 +29,7 @@ class CorreosClient:
         try:
             connection = urllib2.urlopen(WEB_SERVICE_ENDPOINT, params)
             data = connection.read()
-            separated_data = data.split("##")
-            for chunk in separated_data:
-                if chunk.startswith("<?xml"):
-                    return chunk.split("**", 1)[0]
-            return ""
+            return data
         except urllib2.URLError:
             return ""
         except:
@@ -50,47 +46,48 @@ class CorreosParser:
     def __init__(self):
         pass
 
-    def parse(self, xml_text, code):
-        _xml_text = "".join(xml_text).decode('latin_1').encode('utf_8')
-        root = ElementTree.fromstring(_xml_text)
+    def parse(self, html_text, code):
+        soup = BeautifulSoup(html_text)
 
-        root = root.find(".//Envio")
-        if root is None:
+        if soup is None:
             return
 
-        found_code = get_text(root.find("Codigo"), "CodigoEnvio").strip()
+        found_code = self.find_code(soup)
 
         if found_code != code.get_code():
             print >> sys.stderr,\
                 "Code mismatch: expected '{0}', found '{1}'.".format(code.get_code(), found_code)
             return order.NotFoundOrder(code)
 
-        error_code = int(get_text(root, "CodError"))
-        if error_code != 0:
-            return order.NotFoundOrder(code)
+        #error_code = int(get_text(root, "CodError"))
+        #if error_code != 0:
+        #    return order.NotFoundOrder(code)
 
         sent_order = order.SentOrder(code)
-        event_list = root.find("ListaEventos")
-        for xml_event in event_list.iter("Evento"):
-            if len(xml_event) == 0:
+        event_list = soup.findAll('table')[0].findAll('tr')[2:]
+        
+        for html_event in event_list:
+            if len(html_event) == 0:
                 continue
-            date_string = get_text(xml_event, "Fecha")
-            time_string = get_text(xml_event, "Hora")
-            date = time.strptime(date_string + " " + time_string, "%d/%m/%Y %H:%M:%S")
-            text = get_text(xml_event, "Evento")
-            description = get_text(xml_event, "DescripcionWeb")
-
-            city = get_text(xml_event, "Unidad")
-            province = get_text(xml_event, "Provincia")
-            country = get_text(xml_event, "Pais")
-            location = event.Location(country, city, province)
-            new_event = event.Event(date, text, description, location)
+            date_string = html_event.findAll('td')[0].contents
+            date = time.strptime(date_string[0], "\r\t\t\t\t%d/%m/%Y")
+            description_html = html_event.findAll('td')[1]
+            description = description_html.span['title']
+            text =description_html.contents[1].contents[0].split("\r")[0]
+            new_event = event.Event(date, text, description, None)
 
             sent_order.add_event(new_event)
 
         return sent_order
 
-
+    def find_code(self, soup):
+        try:
+            code = soup.findAll('td')[0].contents[0].split(';')[2]
+        except:
+            return ""
+    
+        return code
+    
 def get_text(xml_node, child_name):
     child_node = xml_node.find(child_name)
     if child_node is None:
